3d43a0e4881b18b61e409cc5e7e6dbea250c81b9
Oleg <o.v.plotnikov@gmail.com>
2022-08-09 19:53:07 +0500

    ДЗ С1_Задание1

---
+++ HW_S01_W1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Программа на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите первое число: ");
+
+string? s_num1 = Console.ReadLine();
+
+int num1 = int.Parse(s_num1);
+
+Console.WriteLine("Введите первое число: ");
+
+string? s_num2 = Console.ReadLine();
+
+int num2 = int.Parse(s_num2);
+
+int max = num1;
+    
+
+if (num1 < num2)
+{
+    max = num2;
+
+}
+
+Console.WriteLine("max = " + max);

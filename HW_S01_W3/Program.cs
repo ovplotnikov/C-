e05475e2ad5d9a1e499191012725002fc1ed642e
Oleg <o.v.plotnikov@gmail.com>
2022-08-09 20:23:36 +0500

    ДЗ С1_Задание3

---
+++ HW_S01_W3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+
+string? s_num1 = Console.ReadLine();
+
+int num1 = int.Parse(s_num1);
+
+int div = num1%2;
+
+string answer = ("Число четное");
+
+if (div > 0)
+{
+    answer = ("Число нечетное");
+}
+
+Console.WriteLine(answer);

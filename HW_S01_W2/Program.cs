b14d14ad23486644ef36851cda9ab5f2406f3713
Oleg <o.v.plotnikov@gmail.com>
2022-08-09 20:05:09 +0500

    ДЗ С1_Задание2

---
+++ HW_S01_W2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Программа принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+
+string? s_num1 = Console.ReadLine();
+
+int num1 = int.Parse(s_num1);
+
+Console.WriteLine("Введите второе число: ");
+
+string? s_num2 = Console.ReadLine();
+
+int num2 = int.Parse(s_num2);
+
+Console.WriteLine("Введите третье число: ");
+
+string? s_num3 = Console.ReadLine();
+
+int num3 = int.Parse(s_num3);
+
+int max = num1;
+    
+
+if (num1 < num2)
+{
+    max = num2;
+
+}
+
+if (num2 < num3)
+{
+    max = num3;
+
+}
+Console.WriteLine("max = " + max);

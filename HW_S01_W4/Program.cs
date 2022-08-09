b9091e62d3b2b4350671e4ec665b8e2267bbc37b
Oleg <o.v.plotnikov@gmail.com>
2022-08-09 20:54:48 +0500

    ДЗ С1_Задание4

---
+++ HW_S01_W4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+
+string? s_num1 = Console.ReadLine();
+
+int num1 = int.Parse(s_num1);
+
+for (int i = 1; i <= num1; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);

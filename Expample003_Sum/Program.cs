7d23bd3e0bf9f52c9b77793069cbdc076a63c2d2
Oleg <o.v.plotnikov@gmail.com>
2022-08-09 15:50:13 +0500

    example 004 Div add random

--- Expample003_Sum/Program.cs
+++ Expample003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);;
 int result = numberA + numberB;
-Console.WriteLine(numberA + numberB);
+Console.WriteLine("Первое число");
+Console.WriteLine(numberA);
+Console.WriteLine("Второе число");
+Console.WriteLine(numberB);
+Console.WriteLine("Сумма чисел");
+Console.WriteLine(result);

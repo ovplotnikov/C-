a0561f54a2ce9193b6f697bc5ac8a5451dfda18c
Oleg <o.v.plotnikov@gmail.com>
2022-08-13 21:56:00 +0500

    Array

---
+++ Expample009_Array/Program.cs
@@ -0,0 +1,19 @@
+﻿// Использование массивов
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result=arg2;
+    if(arg3> result) result=arg3;
+    return result;
+}
+
+
+int [] Array = {1,2,3,4,50,6,7,8,9};
+
+
+int max = Max(
+    Max(Array[0], Array[1], Array[2]),
+    Max(Array[3], Array[4], Array[5]),
+    Max(Array[6], Array[7], Array[8])
+);
+Console.WriteLine(max);

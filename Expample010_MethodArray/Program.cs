8de176936c892859096c7868d2f8121d2c5730d7
Oleg <o.v.plotnikov@gmail.com>
2022-08-13 21:56:28 +0500

    MethodArray

---
+++ Expample010_MethodArray/Program.cs
@@ -0,0 +1,24 @@
+﻿// Использование массивов
+int[] Array = { 1, 2, 3, 4, 50, 6, 7, 8, 50 };
+
+int n = Array.Length;
+
+int find = 50;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (Array[index] == find)
+    {
+        Console.WriteLine("Индекс элемента " + index);
+        Console.WriteLine("Значение элемента " + Array[index]);
+        break; //прерывает в после первого совпадения
+    }
+     
+    //index = index+1;
+    index++;
+  
+
+}

b53bfc70da20d1b74fd712ee542a423ce12058e2
Oleg <o.v.plotnikov@gmail.com>
2022-08-13 22:11:26 +0500

    Вотрая цифра трехзначного числа

---
+++ HW_S02_W1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+string? s_num1 = Console.ReadLine();
+
+int num1 = int.Parse(s_num1);
+
+int ThirdDigigt = num1 % 10;
+int FirstDigigt = num1 / 100;
+int SecondDigit = (num1-ThirdDigigt-FirstDigigt*100)/10;
+
+
+Console.WriteLine("Вторая цифра: " + SecondDigit);

20c36ea0e4048284be09671f51e4e446187cdb4a
Oleg <o.v.plotnikov@gmail.com>
2022-08-13 22:30:45 +0500

    Третья цифра числа

---
+++ HW_S02_W2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите любое целое число: ");
+
+string? s_num1 = Console.ReadLine();
+
+int num1 = int.Parse(s_num1);
+
+if (num1 < 100)
+{
+    Console.WriteLine ("Третьей цифры числа нет");
+}
+
+else
+{
+while (num1 > 1000)
+{
+    num1 = num1/10;
+    
+}
+int ThirdDigigt = num1 % 10;
+Console.WriteLine ("Третья цифра числа " + ThirdDigigt);
+}

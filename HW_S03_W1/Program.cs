24eac18930773ce4c3538b14761606a6b9b1dcf8
Oleg <o.v.plotnikov@gmail.com>
2022-08-14 21:01:39 +0500

    Число - палиндром

---
+++ HW_S03_W1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Проверка - является ли число палиндромом");
+
+Console.WriteLine("Введите число");
+
+var numString = Console.ReadLine();
+
+int num = int.Parse(numString!);
+
+int num1 = num;
+
+int rev = 0;
+
+int total = 0;
+
+while (num > 0)
+{
+    rev = num % 10;
+    num = num / 10;
+    total = (total * 10) + rev;
+  }
+if (num1 == total)
+{
+Console.WriteLine("Число - палиндромом");
+}
+else
+{
+Console.WriteLine("Число - НЕ палиндромом");
+}

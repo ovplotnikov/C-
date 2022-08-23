8050560d72445039c6256c6ac38acf36c9f7c926
Oleg <o.v.plotnikov@gmail.com>
2022-08-24 00:07:08 +0600

    Вывод в консоль заданного массива из 8-ми элементов

---
+++ HW_S04_W3/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+int [] Array = {0,0,0,0,0,0,0,0};
+
+Console.WriteLine("Введите 0 элемент массива");
+var String0 = Console.ReadLine();
+Array[0] = int.Parse(String0!);
+
+Console.WriteLine("Введите 1 элемент массива");
+var String1 = Console.ReadLine();
+Array[1] = int.Parse(String1!);
+
+Console.WriteLine("Введите 2 элемент массива");
+var String2 = Console.ReadLine();
+Array[2] = int.Parse(String2!);
+
+Console.WriteLine("Введите 3 элемент массива");
+var String3 = Console.ReadLine();
+Array[3] = int.Parse(String3!);
+
+Console.WriteLine("Введите 4 элемент массива");
+var String4 = Console.ReadLine();
+Array[4] = int.Parse(String4!);
+
+Console.WriteLine("Введите 5 элемент массива");
+var String5 = Console.ReadLine();
+Array[5] = int.Parse(String5!);
+
+Console.WriteLine("Введите 6 элемент массива");
+var String6 = Console.ReadLine();
+Array[6] = int.Parse(String6!);
+
+Console.WriteLine("Введите 7 элемент массива");
+var String7 = Console.ReadLine();
+Array[7] = int.Parse(String7!);
+
+Console.WriteLine("Элемкенты массива по порядку: ");
+Console.Write(Array[0]);
+Console.Write(" " + Array[1]);
+Console.Write(" " + Array[2]);
+Console.Write(" " + Array[3]);
+Console.Write(" " + Array[4]);
+Console.Write(" " + Array[5]);
+Console.Write(" " + Array[6]);
+Console.Write(" " + Array[7]);

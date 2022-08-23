b9ba7a405177152ab3d48ae781f871c9d2331174
Oleg <o.v.plotnikov@gmail.com>
2022-08-23 23:46:11 +0600

    Сумма цифр числа

---
+++ HW_S04_W2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Сумма цифр числа А");
+
+Console.WriteLine("Введите число А");
+
+var аString = Console.ReadLine();
+
+int a = int.Parse(аString!);
+
+int printa = a;
+
+int sum = 0;
+
+while (a > 0)
+{
+    sum = sum + (a % 10);
+
+    a = a / 10;
+
+}
+
+
+Console.WriteLine("Сумма цифр числа " + printa + " = " + sum);

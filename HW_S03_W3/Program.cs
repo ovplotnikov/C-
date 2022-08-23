90291918d297f79615027bea065aa2d12da2006d
Oleg <o.v.plotnikov@gmail.com>
2022-08-23 22:50:13 +0600

    Таблица кубов чисел от 1 до N

---
+++ HW_S03_W3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Таблица кубов чисел от 1 до N");
+
+Console.WriteLine("Введите число N");
+
+var nString = Console.ReadLine();
+
+double n = int.Parse(nString!);
+double power3 = 3;
+Console.WriteLine(Math.Pow(n, power3));
+
+while (n > 1)
+{
+    n = n - 1;
+    Console.WriteLine(Math.Pow(n, power3));
+}

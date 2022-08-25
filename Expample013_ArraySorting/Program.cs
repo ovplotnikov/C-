abc16c46740608043d2f6bb268dada9a7bd98b19
Oleg <o.v.plotnikov@gmail.com>
2022-08-25 22:02:07 +0600

    Метод для сортировки элементов масиива от меньшего к большему

---
+++ Expample013_ArraySorting/Program.cs
@@ -0,0 +1,32 @@
+﻿// сортировка элементов массива от меньшего к большему
+
+int[] arr = { 2, 3, 7, 9, 4, 6, 8, 1, 5 };
+void PtintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void selectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PtintArray(arr);
+selectionSort(arr);
+PtintArray(arr);

d7ff53f42a66bc9eec48536f51ec8352779f5cec
Oleg <o.v.plotnikov@gmail.com>
2022-08-26 22:20:31 +0600

    Определяем сколько введено числел >0

---
+++ HW_S06_W1/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 0;
+int i = 0;
+char b = ' ';
+
+Console.WriteLine("Введите символ и нажмите <Enter>");
+Console.WriteLine("Для завершения нажмите точку '.' и нажмите <Enter>");
+
+do
+{
+    Console.Write("-> ");
+    string s = Console.ReadLine();
+    Int32.TryParse(s, out a);
+    char.TryParse (s, out b);
+    
+
+    //a = Convert.ToDouble(s);
+    if (a > 0)
+    {
+        i++;
+    }
+    Console.WriteLine("Чисел больше 0 уже " + i);
+}
+while (b != '.');
+
+Console.WriteLine("Было введено чисел, которые больше  0: " + i);

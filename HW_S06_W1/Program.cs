9faa490e587f98f22a5a37a35c0e026dbb60c0ba
Oleg <o.v.plotnikov@gmail.com>
2022-08-28 14:40:47 +0600

    Введено чисел >0 (v.2.0)

--- HW_S06_W1/Program.cs
+++ HW_S06_W1/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int a = 0;
+/* int a = 0;
 int i = 0;
 char b = ' ';
 
@@ -24,4 +24,25 @@ do
 }
 while (b != '.');
 
-Console.WriteLine("Было введено чисел, которые больше  0: " + i);
+Console.WriteLine("Было введено чисел, которые больше  0: " + i); */
+
+int count = 0;
+string str;
+Console.WriteLine("Введите символ и нажмите <Enter>");
+Console.WriteLine("Для завершения нажмите точку '.' и нажмите <Enter>");
+do
+{
+Console.Write("-> ");
+str = Console.ReadLine()!;
+if (int.TryParse(str, out int number))
+{
+if (number > 0)
+{
+count++;
+}
+}
+Console.WriteLine("Чисел больше 0 уже " + count);
+}
+// значение не равно точке и не пустое
+while (!str.Equals('.') && !string.IsNullOrEmpty(str));
+Console.WriteLine("Было введено чисел, которые больше 0: " + count);

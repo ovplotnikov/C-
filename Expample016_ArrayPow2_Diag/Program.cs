e627c73410f29d73c7945c9ae4c4cb8bfd3e56da
Oleg <o.v.plotnikov@gmail.com>
2022-08-29 23:49:23 +0600

    диагональ матрицы в степень 2

---
+++ Expample016_ArrayPow2_Diag/Program.cs
@@ -0,0 +1,49 @@
+﻿/*  Задача 49: Задайте двумерный массив. 
+Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+ 1 4 7 2
+ 5 9 2 3
+ 8 4 2 4
+ */
+
+
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] result = new int[m, n];
+    var random = new Random();
+    for (var i = 0; i < result.GetLength(0); i++)
+        for (var j = 0; j < result.GetLength(1); j++)
+            result[i,j] = random.Next(1, 10);
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++) Console.Write($"{array[i, j]}, ");
+        Console.WriteLine();
+    }
+}
+int [,] ProductArray (int [,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        if (i%2==0 && j%2==0) array [i,j] = (array[i,j]*array[i,j]); 
+    }    
+    return array;
+}
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m)) Console.WriteLine("Всё плохо");
+
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n)) Console.WriteLine("Всё плохо");
+int [,] array =CreateArrayWithRandomNumbers(m, n);
+PrintArray (array);
+Console.WriteLine ();
+PrintArray(ProductArray (array));
+
+

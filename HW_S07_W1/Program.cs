c3d357dd6f95fb2e8507429a7838fcb0ac3abc96
Oleg <o.v.plotnikov@gmail.com>
2022-08-29 23:55:30 +0600

    двумерный массив размером mxn

---
+++ HW_S07_W1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:F2} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    double[,]  array = new double[m, n];
+    var random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.NextDouble()*10-5;
+        }
+    }
+    return array;
+
+}
+
+
+
+
+Console.WriteLine("Введите число строк (m)");
+if(!int.TryParse(Console.ReadLine()!, out var m)) {
+    Console.WriteLine("Некорртеный ввод");
+}
+
+Console.WriteLine("Введите число столбцов (n)");
+if(!int.TryParse(Console.ReadLine()!, out var n)) {
+    Console.WriteLine("Некорртеный ввод");
+}
+
+PrintArray(CreateArrayWithRandomNumbers(m, n));

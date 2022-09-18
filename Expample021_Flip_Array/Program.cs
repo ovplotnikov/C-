312e0cb71f5bc1f02e5cf8d4c88f723b210ccbdd
Oleg <o.v.plotnikov@gmail.com>
2022-09-18 20:13:14 +0600

    Flip_Array

---
+++ Expample021_Flip_Array/Program.cs
@@ -0,0 +1,55 @@
+﻿// Программа, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    var random = new Random();
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+
+    return array;
+}
+
+
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m)) Console.WriteLine("Всё плохо");
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n)) Console.WriteLine("Всё плохо");
+
+
+int[,] array = CreateArrayWithRandomNumbers(m, n);
+
+PrintArray(array);
+
+for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                Array.Reverse(array, i, array.GetLength(0));
+            }
+        }
+        Console.WriteLine();
+    }
+
+PrintArray(array);

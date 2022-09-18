c64847af278f7724069ffb74d04dc360118a8cfc
Oleg <o.v.plotnikov@gmail.com>
2022-09-18 20:11:47 +0600

    Fibonacci_Numbers

---
+++ Expample019_Fibonacci_Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вычисление чисел Фибоначчи до заданного порядка
+Console.WriteLine("Введите порядок чисел Фибоначчи:");
+
+int Fibonacci (int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+double n = double.Parse(Console.ReadLine());
+
+for (int i = 0; i <= n; i++)
+{
+    Console.WriteLine($"{i}-е число Фибоначчи: {Fibonacci(i)}");
+}
